f7b3d50068e3a2b09adbf266c62d84757b9962ce
sanctorium <otero_jose@hotmail.com>
2017-06-11 23:28:52 +0200

    The first blank window of the "Particle Fire Explosion" is done.

--- cTutorial.pro
+++ cTutorial.pro
@@ -4,3 +4,10 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.cpp
+
+# Setting access up to the SDL (Simple Directmedia Layer) library. -jmor
+# Adding libraries path: -L[Directory path of "lib" folder] -lSDL2
+LIBS += -L C:/Users/otero/Documents/SDL2-2.0.5/x86_64-w64-mingw32/lib -lSDL2
+#LIBS += -L C:/Users/otero/Documents/SDL2-2.0.5/x86_64-w64-mingw32/lib -lSDL2main -lSDL2
+# Adding include path: [Directory of "include"]
+INCLUDEPATH += C:/Users/otero/Documents/SDL2-2.0.5/x86_64-w64-mingw32/include/SDL2
--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const int WINDOW_WIDTH {800};
+    const int WINDOW_HEIGHT {600};
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "SDL init failed!!!" << endl;
+        return 1;
+    }
+
+    SDL_Window* window {SDL_CreateWindow("Particle Fire Explosion",
+                                         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                         WINDOW_WIDTH, WINDOW_HEIGHT,
+                                         SDL_WINDOW_SHOWN)};
+    if(window == nullptr) {
+        SDL_Quit();
+        return 2;
+    }
+
+    SDL_Event event;
+
+    bool quit {false};
+    while(!quit) {
+        //
+        while(SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
